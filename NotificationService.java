ff233215e2d9cf8a7c71f595e0e1ae2afaab8bed
Mahesh R M <maheshrmadiwalar@gmail.com>
2025-11-02 13:18:56 +0530

    Add NotificationService module

---
+++ NotificationService.java
@@ -0,0 +1,6 @@
+// Implementation of NotificationService
+public class NotificationService {
+    public NotificationService() {
+        System.out.println("NotificationService initialized.");
+    }
+}
