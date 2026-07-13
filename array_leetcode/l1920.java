e335782c67a5f2f7f48be2ff838a000c1b237017
Mahesh R M <maheshrmadiwalar@gmail.com>
2026-07-13 22:35:38 +0530

    pushing the leetcode problem 1920

---
+++ array_leetcode/l1920.java
@@ -0,0 +1,28 @@
+
+public class l1920 {
+
+    // p no : 1920
+//    Input: nums = [0,2,1,5,3,4]
+// Output: [0,1,2,4,5,3]
+// Explanation: The array ans is built as follows: 
+// ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
+//     = [nums[0], nums[2], nums[1], nums[5], nums[3], nums[4]]
+//     = [0,1,2,4,5,3]
+    public static int[] buildArray(int[] nums) {
+        int n = nums.length;
+        int ans[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 2, 1, 5, 3, 4};
+        int[] ans = buildArray(nums);
+        for (int x : ans) {
+            System.out.println(x);
+        }
+
+    }
+}
