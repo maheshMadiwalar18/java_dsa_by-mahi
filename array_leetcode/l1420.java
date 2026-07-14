d57f132592b2a9abc3ac6cb7a54f0d8de7ede4ae
Mahesh R M <maheshrmadiwalar@gmail.com>
2026-07-14 10:08:35 +0530

    The leetcode 1672  is submitted

--- array_leetcode/l1420.java
+++ array_leetcode/l1420.java
@@ -1,1 +1,12 @@
 //1480. Running Sum of 1d Array
+
+class l1420 {
+
+    public int[] runningSum(int[] nums) {
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            nums[i] = nums[i] + nums[i - 1];
+        }
+        return nums;
+    }
+}
